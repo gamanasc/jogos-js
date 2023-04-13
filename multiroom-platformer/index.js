856c564106666b1d3a432e4f7b84a19597ba0bc2
Lucas Gama <lucasgdon@gmail.com>
2023-04-12 22:07:04 -0300

    Criando a classe Player com métodos draw e update básicos

--- multiroom-platformer/index.js
+++ multiroom-platformer/index.js
@@ -6,3 +6,17 @@ canvas.height =  64 * 9 // 576
 
 c.fillStyle = 'white';
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+const player = new Player()
+
+function animate(){
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'white';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+
+    player.draw()
+    player.update()
+
+}
+
+animate()
